a7e20355ad4b58fede2636e1a26dfc62dad39b3b
Bernhard Tittelbach <bernhard@tittelbach.org>
2024-04-03 17:22:44 +0200

    gewohnheiten

--- chrumm/leds68/keymaps/btittelbach_65keys_mouse_rgblight/keymap.c
+++ chrumm/leds68/keymaps/btittelbach_65keys_mouse_rgblight/keymap.c
@@ -18,8 +18,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_LCTL, OSL(1),  KC_LGUI, KC_LALT, KC_SPC,  KC_BSPC, KC_ENT,  KC_SPC,   KC_RALT,  KC_DEL,  KC_LEFT,  KC_DOWN, KC_RIGHT
     ),
     [_FN] = LAYOUT(
-        KC_GRV,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_P7,    KC_P8,    KC_P9,   KC_PMNS,  KC_BSPC, KC_DEL,
-        MO(2),   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_P4,    KC_P5,    KC_P6,   KC_PPLS,  KC_LPRN, KC_RPRN,
+        KC_GRV,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_P7,    KC_P8,    KC_P9,   KC_PMNS,  KC_BSPC, KC_F12,
+        MO(2),   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_P4,    KC_P5,    KC_P6,   KC_PPLS,  KC_DEL,  KC_ENT,
         _______, KC_APP,  KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, XXXXXXX, KC_P1,    KC_P2,    KC_P3,   KC_PSLS,  XXXXXXX, _______,
         _______, KC_NUBS, KC_BTN2, KC_BTN3, KC_BTN1, KC_MUTE, XXXXXXX, KC_P0,    KC_P0,    KC_PDOT, KC_PAST,  KC_PGUP, _______,
         _______, _______, _______, _______, _______, _______, _______, _______,  _______,  KC_INS,  KC_HOME,  KC_PGDN, KC_END
